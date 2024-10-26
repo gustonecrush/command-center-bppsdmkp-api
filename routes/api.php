602adc1f93c6eb0cf40c63559230bd46e08cf392
gustonecrush <codeboxezz@gmail.com>
2024-10-27 00:01:55 +0700

    fix api pendidik

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\PendidikController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/pendidiks', [PendidikController::class, 'index']);
