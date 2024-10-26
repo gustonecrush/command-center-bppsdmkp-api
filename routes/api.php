cdecade28b0bd2634551500bd0316411c9649e10
gustonecrush <codeboxezz@gmail.com>
2024-10-27 00:25:09 +0700

    fix api route

--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,4 @@ use App\Http\Controllers\Api\PendidikController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/pendidiks', [PendidikController::class, 'index']);
+Route::apiResource('/pendidiks', PendidikController::class);
