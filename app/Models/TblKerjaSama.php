08ec62e979dc390c014af30ae3320ce38442b13d
gustonecrush <codeboxezz@gmail.com>
2025-07-05 00:41:56 +0700

    push

--- app/Models/TblKerjaSama.php
+++ app/Models/TblKerjaSama.php
@@ -10,6 +10,27 @@ class TblKerjaSama extends Model
     use HasFactory;
 
     protected $table = "tbl_kerjasama";
-
     public $timestamps = false;
+
+    protected $fillable = [
+        'Judul_Kerja_Sama',
+        'Ruang_Lingkup',
+        'Substansi',
+        'Pemrakarsa',
+        'Jenis_Dokumen',
+        'Lingkup',
+        'Tingkatan',
+        'Pihak_KKP',
+        'Pihak_Mitra',
+        'Informasi_Penandatanganan',
+        'Mulai',
+        'Selesai',
+        'Pembiayaan',
+        'Keterangan',
+        'File_Dokumen',
+        'Created_By',
+        'When_Created',
+        'Updated_By',
+        'When_Updated',
+    ];
 }
