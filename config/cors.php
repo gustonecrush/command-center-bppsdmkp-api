f245038f29c2e5a337a81bda7a58c47b278f7686
gustonecrush <codeboxezz@gmail.com>
2025-11-17 13:05:52 +0700

    push

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://elaut-bppsdm.kkp.go.id', 'http://localhost', 'https://command-center-bppsdmkp.vercel.app', 'http://localhost:6323', 'https://sikap-dpkakp-dev.vercel.app'],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
